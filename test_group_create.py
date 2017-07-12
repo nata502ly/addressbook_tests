72c35e2cb5844940927b55820ddcd5e39d87f602
Olena Piankova <el.piankova@gmail.com>
2017-07-12 19:50:18 +0300

    Add comments

--- test_group_create.py
+++ test_group_create.py
@@ -1,16 +1,17 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 import unittest
 
 
 class test_group_create(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(60)
     
     def test_test_group_create(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost:8888/addressbook/index.php")
+        # Open main page
+        wd.get("http://localhost/addressbook/index.php")
+        # Login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -18,7 +19,9 @@ class test_group_create(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("Группы").click()
+        # Open group page
+        wd.find_element_by_css_selector("#nav > ul > li.admin > a").click()
+        # Create
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -32,12 +35,13 @@ class test_group_create(unittest.TestCase):
         if not wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").click()
         wd.find_element_by_name("submit").click()
+        # TODO: Verify message
+        # Return to group page
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Выйти").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\9")
-        self.assertTrue(success)
-    
+        # Logout
+        wd.find_element_by_css_selector("#top > form > a").click()
+        # TODO: Verify group created
+
     def tearDown(self):
         self.wd.quit()
 
