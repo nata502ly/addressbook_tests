bd4c34f08c5b9922b5f72b0790db3a21d53e60b7
Olena Piankova <el.piankova@gmail.com>
2017-07-14 13:19:39 +0300

    Change the order of methods in AddressbookAPI for convinience

--- addressbook_api.py
+++ addressbook_api.py
@@ -6,13 +6,29 @@ class AddressBook:
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(60)
 
-    def message(self):
-        return self.wd.find_element_by_css_selector('#content > div').text
+    def open_main_page(self):
+        self.wd.get("http://localhost/addressbook/index.php")
 
+    def login(self, username, password):
+        wd = self.wd
+        # Fill form login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        # Submit login
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def logout(self):
         # Logout
         self.wd.find_element_by_css_selector("#top > form > a").click()
+
+    def open_group_page(self):
+        wd = self.wd
+        # Open group page
+        wd.find_element_by_css_selector("#nav > ul > li.admin > a").click()
 
     def return_to_group_page(self):
         wd = self.wd
@@ -32,29 +48,12 @@ class AddressBook:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").click()
+        # if not wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").is_selected():
+        #     wd.find_element_by_xpath("//div[@id='content']/form/select//option[11]").click()
         wd.find_element_by_name("submit").click()
 
-    def open_group_page(self):
-        wd = self.wd
-        # Open group page
-        wd.find_element_by_css_selector("#nav > ul > li.admin > a").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        # Fill form login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        # Submit login
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_main_page(self):
-        self.wd.get("http://localhost/addressbook/index.php")
+    def message(self):
+        return self.wd.find_element_by_css_selector('#content > div').text
 
     def destroy(self):
         self.wd.quit()
