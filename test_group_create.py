be42bc7cc2df40f8a320a5ebc8ba4edcc4dd5686
Olena Piankova <el.piankova@gmail.com>
2017-07-14 13:31:40 +0300

    Extracted login/logout to separete fixture

--- test_group_create.py
+++ test_group_create.py
@@ -1,14 +1,11 @@
 from models.group import Group
 
 
-def test_group_create(app):
+def test_group_create(app, init_login):
     group = Group(name="00000090000", header="00000", footer="000000")
-    app.open_main_page()
-    app.login(username="admin", password="secret")
     app.open_group_page()
     app.create_group(group)
     assert "A new group has been entered into the address book." in app.message()
     app.return_to_group_page()
-    app.logout()
     # TODO: Verify group created
 
--- test_group_delete.py
+++ test_group_delete.py
@@ -1,9 +1,6 @@
-def test_group_create(app):
-    app.open_main_page()
-    app.login(username="admin", password="secret")
+def test_group_create(app, init_login):
     app.open_group_page()
     app.delete_first_group()
     assert "Group has been removed." in app.message()
     app.return_to_group_page()
     # TODO: Verify group deleted
-    app.logout()
