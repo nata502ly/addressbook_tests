43ad91ea8b96da9781f8607a704b650a2c4f966c
Olena Piankova <el.piankova@gmail.com>
2017-07-26 13:45:12 +0300

    Test of deleting was modified for DB cheking

--- tests/test_group_delete.py
+++ tests/test_group_delete.py
@@ -1,10 +1,14 @@
 import random
 
 
-def test_group_create(app, init_login, init_group):
+def test_group_delete(app, init_login, init_group, db):
+    old_group_list = db.get_group_list()
     index = random.randrange(app.group.count())
     app.open_group_page()
     app.group.delete(index)
     assert "Group has been removed." in app.message()
     app.return_to_group_page()
-    # TODO: Verify group deleted
+    new_group_list = db.get_group_list()
+    assert len(old_group_list) - 1 == len(new_group_list)
+    old_group_list.pop(index)
+    assert old_group_list == new_group_list
