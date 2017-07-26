de84731e775a3b54a9ec4fb491df20b7433b0cc9
Olena Piankova <el.piankova@gmail.com>
2017-07-26 14:07:30 +0300

    BDD tests added

---
+++ bdd_tests/group_scenarios.py
@@ -0,0 +1,12 @@
+from pytest_bdd import scenario
+from bdd_tests.group_steps import *
+
+
+@scenario("groups.feature", "Add new group")
+def test_add_group():
+    pass
+
+
+@scenario("groups.feature", "Delete a random group")
+def test_delete_random_group():
+    pass
